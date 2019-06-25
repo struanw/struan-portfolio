ba0b178565cc32eb80260583c83f02acb633714a
Struan-Wall <struan03@hotmail.com>
2019-06-25 14:56:09 +1200

    set up initial components displaying hello world

---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
+
+class Main extends React.Component {
+    state = { }
+
+
+
+    render () {
+      return (
+      <>
+        <Row>
+          <Col>
+           <h1>appy app </h1>
+          </Col>
+          <Col>
+           
+          </Col>
+        </Row>
+      </>
+      )
+    }
+}
+
+export default Main
